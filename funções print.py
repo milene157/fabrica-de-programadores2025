d7973e57ec849865f489fc617801dad530701868
milene157 <milenebeatriz2008@gmail.com>
2025-05-08 15:51:04 -0300

    função print
    Aula do dia 08 de maio

---
+++ funções print.py
@@ -0,0 +1,31 @@
+texto_escrito="este livro é legal!"
+outro_texto='vou aprender a programar.'
+outro_texto=texto_escrito
+#print(outro_texto) 
+primeiro_nome='milene'
+segundo_nome='beatriz'
+nome_completo=primeiro_nome+segundo_nome
+#print(nome_completo)
+saudacao='olá '+primeiro_nome
+#print(saudacao)
+balas_subtraídas=3
+quantidade_balas=20
+quantidade_balas-=balas_subtraídas
+#print(balas_subtraídas)
+quantidade_balas=20
+balas_subtraídas=3
+quantidade_balas*=balas_subtraídas
+#print(quantidade_balas)
+nome=input()
+saudacao='olá '+nome
+#print(saudacao)
+numero_string=input(4)
+numero_inteiro=int(6)
+subtracao=numero_inteiro-2
+print(subtracao)
+numero_um=input()
+numero_dois=input()
+resultado_soma=int(1)numero_um+int(2)numero_dois
+print(resultado_soma)
+mensagem="o resultado da soma é: "+resultado_soma_texto
+print(mensagem)
